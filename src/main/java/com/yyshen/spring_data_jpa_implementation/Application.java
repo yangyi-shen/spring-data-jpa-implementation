59f4ecb7e199f840986d03fa378c06dbf126423c
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-02-12 20:28:59 +0800

    feat: initialize spring boot project

---
+++ src/main/java/com/yyshen/spring_data_jpa_implementation/Application.java
@@ -0,0 +1,13 @@
+package com.yyshen.spring_data_jpa_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yyshen/spring_data_jpa_implementation/Application.java
@@ -0,0 +1,13 @@
+package com.yyshen.spring_data_jpa_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
