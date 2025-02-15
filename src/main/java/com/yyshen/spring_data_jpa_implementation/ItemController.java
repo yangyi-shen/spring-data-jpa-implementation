84964ad78641e70258a731456e9969177d9d5292
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-02-15 15:28:19 +0800

    feat: item controller with basic functions

---
+++ src/main/java/com/yyshen/spring_data_jpa_implementation/ItemController.java
@@ -0,0 +1,40 @@
+package com.yyshen.spring_data_jpa_implementation;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+    private final static Item NULL = new Item("NULL");
+    private final static Random RANDOMIZER = new Random();
+
+    private final ItemRepository repository;
+
+    public ItemController(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/api")
+    public List<ItemResource> getAll() {
+        return this.repository.findAll().stream()
+            .map(item -> new ItemResource("success", item))
+            .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/{id}")
+    public ItemResource getItem(@PathVariable Long id) {
+        return repository.findById(id)
+            .map(item -> new ItemResource("success", item))
+            .orElse(new ItemResource("failure: item with ID " + id + "does not exist", NULL));
+    }
+
+    @GetMapping("/api/random")
+    public ItemResource getRandom() {
+        return getItem(RANDOMIZER.nextLong(1, repository.count() + 1));
+    }
+}
