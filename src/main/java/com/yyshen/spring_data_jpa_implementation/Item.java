5cbebc2c454dff88e5d52d521d0a6635fab88687
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-02-15 15:27:35 +0800

    feat: create item class

---
+++ src/main/java/com/yyshen/spring_data_jpa_implementation/Item.java
@@ -0,0 +1,57 @@
+package com.yyshen.spring_data_jpa_implementation;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+
+    Item(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        } else if (!(object instanceof Item)) {
+            return false;
+        }
+        Item that = (Item) object;
+        return Objects.equals(this.id, that.id) && Objects.equals(this.text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.text);
+    }
+
+    @Override
+    public String toString() {
+        return "Item[id=" + this.id + ", text=" + this.text + "]";
+    }
+}
